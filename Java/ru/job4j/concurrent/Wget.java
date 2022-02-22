cf9acb69f2fefe05cce01523120ac80a8405b3ea
maD-skillz <warneverchang3s@gmail.com>
2022-02-22 13:29:49 +0300

    4. Скачивание файла с ограничением. [#144271 #267820]

--- Java/ru/job4j/concurrent/Wget.java
+++ Java/ru/job4j/concurrent/Wget.java
@@ -3,31 +3,39 @@ package ru.job4j.concurrent;
 import java.io.BufferedInputStream;
 import java.io.*;
 import java.net.URL;
-import java.sql.Timestamp;
 
 public class Wget implements Runnable {
 
     private final String url;
     private final int speed;
+    private final String fileName;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-        FileOutputStream fileOutputStream = new FileOutputStream("output_file_wget.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             byte[] buffer = new byte[1024];
             int bytesRead;
+            int downloadData = 0;
             long startTime = System.currentTimeMillis();
-            while((bytesRead = in.read(buffer, 0, 1024)) != -1) {
+            while ((bytesRead = in.read(buffer, 0, 1024)) != -1) {
                 fileOutputStream.write(buffer, 0, bytesRead);
-                long afterTime = System.currentTimeMillis();
-                if (speed < bytesRead / startTime - afterTime) {
-                    Thread.sleep(speed);
+                downloadData = bytesRead;
+                if (downloadData == speed) {
+                    long afterTime = System.currentTimeMillis();
+                    if ((afterTime - startTime) < 1000) {
+                        Thread.sleep(1000 - (afterTime - startTime));
+                        downloadData = 0;
+                        Thread.currentThread().start();
+                    }
                 }
+
             }
         } catch (InterruptedException | IOException e) {
             Thread.currentThread().interrupt();
@@ -35,10 +43,15 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
-        wget.start();
-        wget.join();
+        if (args[0].length() == 0 || args[1].length() == 0 || args[2].length() == 0) {
+            throw new IllegalArgumentException();
+        } else {
+            String url = args[0];
+            int speed = Integer.parseInt(args[1]);
+            String fileName = args[2];
+            Thread wget = new Thread(new Wget(url, speed, fileName));
+            wget.start();
+            wget.join();
+        }
     }
 }
