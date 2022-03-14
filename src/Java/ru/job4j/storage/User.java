ef97973e9d2a96fa7435b69790a6c709329cfa00
maD-skillz <warneverchang3s@gmail.com>
2022-03-14 21:58:28 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #275046]

--- src/Java/ru/job4j/storage/User.java
+++ src/Java/ru/job4j/storage/User.java
@@ -1,25 +1,32 @@
 package ru.job4j.storage;
 
+import net.jcip.annotations.GuardedBy;
 
 public final class User {
 
+    @GuardedBy("this")
     private final int id;
 
+    @GuardedBy("this")
     private int amount;
+
 
     public User(int id, int amount) {
         this.id = id;
         this.amount = amount;
     }
 
+    @GuardedBy("this")
     public int getId() {
         return id;
     }
 
+    @GuardedBy("this")
     public int getAmount() {
         return amount;
     }
 
+    @GuardedBy("this")
     public void setAmount(int amount) {
         this.amount = amount;
     }
