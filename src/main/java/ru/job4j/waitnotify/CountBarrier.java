93581d77e01607dbbd8a9252908ea8d8b3b89d24
maD-skillz <warneverchang3s@gmail.com>
2022-03-15 19:42:50 +0300

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/waitnotify/CountBarrier.java
@@ -0,0 +1,36 @@
+package ru.job4j.waitnotify;
+
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+        while (count < total) {
+            count++;
+            }
+            notifyAll();
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count >= total) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            notifyAll();
+        }
+    }
+
+}
