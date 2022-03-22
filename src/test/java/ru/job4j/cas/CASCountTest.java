b306c105cb10d296aba3c92908e0b518f6f0a038
maD-skillz <warneverchang3s@gmail.com>
2022-03-22 09:21:54 +0300

    0. CAS - операции [#6859 #276841]

--- src/test/java/ru/job4j/cas/CASCountTest.java
+++ src/test/java/ru/job4j/cas/CASCountTest.java
@@ -3,58 +3,48 @@ package ru.job4j.cas;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.IntStream;
-
-import static org.hamcrest.Matchers.is;
 
 
 import static org.junit.Assert.*;
 
 public class CASCountTest {
 
-    @Ignore
     @Test
     public void countTest() throws InterruptedException {
         final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
         CASCount casCount = new CASCount();
 
-        Thread increment = new Thread(
+        Thread thread1 = new Thread(
                 () -> {
                     try {
                         for (int i = 0; i < 5; i++) {
                             casCount.increment();
                         }
                     } catch (Exception e) {
-                        Thread.currentThread().interrupt();
+                        e.printStackTrace();
                     }
                 }
-        );
 
-        Thread consumer = new Thread(
+        );
+        thread1.start();
+
+        Thread thread2 = new Thread(
                 () -> {
-                    while (!buffer.isEmpty() && !Thread.currentThread().isInterrupted()) {
-                        buffer.add(casCount.getVal());
+                    try {
+                        for (int i = 0; i < 5; i++) {
+                            casCount.increment();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
-        );
-        increment.start();
-        consumer.start();
-        increment.join();
-        consumer.join();
+                );
+        thread2.start();
+        thread1.join();
+        thread2.join();
 
-        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
-    }
-
-    @Test
-    public void test() {
-        CASCount casCount = new CASCount();
-        for (int i = 0; i < 10; i++) {
-            casCount.increment();
-        }
-        assertEquals(10, casCount.getVal());
+        assertEquals(casCount.getVal(), 10);
     }
 
 }
