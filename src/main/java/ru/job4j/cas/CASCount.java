8b53884abdd3668851eee992ccf414bef768efac
maD-skillz <warneverchang3s@gmail.com>
2022-03-31 11:06:44 +0300

    0. CAS - операции [#6859 #276841]

---
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -0,0 +1,22 @@
+package ru.job4j.cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>(0);
+
+
+    public void increment() {
+        int x;
+        do {
+            x = getVal();
+        } while (!count.compareAndSet(x, x + 1));
+    }
+
+    public int getVal() {
+        return count.get();
+    }
+}
