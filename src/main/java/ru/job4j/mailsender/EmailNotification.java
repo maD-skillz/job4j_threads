08dfe2abbe2075e85c75de8ed6834adfde2ec45d
maD-skillz <warneverchang3s@gmail.com>
2022-04-02 19:36:31 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/mailsender/EmailNotification.java
@@ -0,0 +1,48 @@
+package ru.job4j.mailsender;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors());
+
+
+    public void emailTo(User user) throws InterruptedException {
+       pool.submit(new Runnable() {
+           @Override
+           public void run() {
+                if (user.getEmail() != null) {
+                    send(
+                            new StringBuilder()
+                                    .append("Notification ")
+                                    .append(user.getName())
+                                    .append(" to email ")
+                                    .append(user.getEmail()).toString(),
+                            new StringBuilder()
+                                    .append(" Add a new event to ")
+                                    .append(user.getName()).toString(),
+                            user.getEmail()
+                            );
+                }
+           }
+       });
+       close();
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    public void close() throws InterruptedException {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
