8d40222f251ca0d60505d72c2134f6affc1702d9
maD-skillz <warneverchang3s@gmail.com>
2022-04-11 17:01:49 +0300

    Fix

--- src/main/java/ru/job4j/waitnotify/ParallelSearch.java
+++ src/main/java/ru/job4j/waitnotify/ParallelSearch.java
@@ -1,35 +1,40 @@
 package ru.job4j.waitnotify;
 
-
 public class ParallelSearch {
 
-        public static void main(String[] args) {
-            SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(5);
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
             final Thread consumer = new Thread(
                     () -> {
                         while (!Thread.currentThread().isInterrupted()) {
                             try {
                                 System.out.println(queue.poll());
                             } catch (InterruptedException e) {
-                                e.printStackTrace();
-                                Thread.currentThread().interrupt();
+                               Thread.currentThread().interrupt();
                             }
+                        }
+
+                    }
+            );
+
+            final Thread producer = new Thread(
+                    () -> {
+                        for (int index = 0; index != 3; index++) {
+                                queue.offer(index);
+                                try {
+                                    Thread.sleep(200);
+                                } catch (InterruptedException e) {
+                                    Thread.currentThread().interrupt();
+                                }
                         }
                     }
             );
-            consumer.start();
-            new Thread(
-                    () -> {
-                        for (int index = 0; index != 3; index++) {
-                            queue.offer(index);
-                            try {
-                                Thread.sleep(300);
-                            } catch (InterruptedException e) {
-                                Thread.currentThread().interrupt();
-                                }
-                            }
-                            consumer.interrupt();
-                        }
-                    ).start();
-        }
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+
+
+
     }
+}
